73603ae36cd8f5d0fa2af074ef033a92108825b9
k-lange <mail@klange.org>
2015-11-22 23:57:24 +0100

    refactor(projectList): simplify

--- src/components/project-list.jsx
+++ src/components/project-list.jsx
@@ -4,22 +4,17 @@ import { Link } from 'react-router';
 import { categoriesMap } from 'models/projects';
 
 class ProjectList extends Component {
-    getCategory() {
-        return this.props.params.category;
-    }
-
-    getProjects() {
-        return categoriesMap[this.getCategory()];
-    }
-
     render() {
+        const category = this.props.params.category;
+        const projects = categoriesMap[category];
+        
         return (
             <div>
                 <menu className='projects'>
-                    {this.getProjects().map(project => {
+                    {projects.map(project => {
                         return (
                             <li key={project.id}>
-                                <Link activeClassName="active" to={`/${this.getCategory()}/${project.id}`}>
+                                <Link activeClassName="active" to={`/${category}/${project.id}`}>
                                     {project.content.title}
                                 </Link>
                             </li>
